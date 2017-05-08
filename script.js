4fa2fbb5de33a962d5743acb5cc60e143e6e421d
Millie Walsh <millie@primeacademy.io>
2017-05-08 14:25:25 -0500

    name this

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ console.log('this is it');
 var myApp =angular.module( 'myApp', [] );
 
 myApp.controller('myFirstController', function() {
-  this.test = 'This is my first thing';
+  // view model
+  var vm = this;
+  vm.oneThing = 'This is my first thing';
+  vm.twoThing = 'This is my second thing';
+
+  var sillyString = 'heyyyyy ';
+  vm.bobString = addBob(sillyString);
+
+  function addBob(aString) {
+    console.log(this);
+    return aString + 'Bob!';
+  }
 });
