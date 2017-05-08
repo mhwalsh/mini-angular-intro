407bdf64f68c2990e9a305d8de0237649c0bdc4e
Millie Walsh <millie@primeacademy.io>
2017-05-08 14:52:17 -0500

    add two way binding example

--- script.js
+++ script.js
@@ -8,11 +8,6 @@ myApp.controller('myFirstController', function() {
   vm.oneThing = 'This is my first thing';
   vm.twoThing = 'This is my second thing';
 
-  var sillyString = 'heyyyyy ';
-  vm.bobString = addBob(sillyString);
-
-  function addBob(aString) {
-    console.log(this);
-    return aString + 'Bob!';
-  }
+  // two way binding example
+  vm.something = '';
 });
